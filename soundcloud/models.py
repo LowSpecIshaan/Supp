1b0af888dfcbde4825bc21fbc0f7f60c1db92839
Ishaan Gupta <ishaangupta3110@gmail.com>
2026-02-14 11:41:44 +0530

    Changed settings for deployment for 323455th time

--- soundcloud/models.py
+++ soundcloud/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class SoundCloudToken(models.Model):
     user = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    refresh_token = models.CharField(max_length=50)
+    refresh_token = models.TextField()
     expires_in = models.DateTimeField()
-    access_token = models.CharField(max_length=50)
+    access_token = models.TextField()
